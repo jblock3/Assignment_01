946c7250bc5092767857440ae765ee2e358c4c3f
jblock3 <block.jamie123@gmail.com>
2017-01-25 19:39:55 -0500

    Added

---
+++ jblock3_Assign1.py
@@ -0,0 +1,8 @@
+# Initial Prompts
+
+renterName = input('Please enter your full name: ')
+classCode = (input('Please enter your rental classification code (i.e. B for budget, D for Daily, and W for weekly): ')).upper()
+daysRented = int(input('How many days did you rent your vehicle for? '))
+initOdometer = int(input('What was your vehicle\'s\ initial odometer reading? '))
+finOdometer = int(input('What was your vehicle\'s\ final odometer reading? '))
+
